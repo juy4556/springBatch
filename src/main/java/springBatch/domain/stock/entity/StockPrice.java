0682f9b5deeb5246115e453c346e86585e817000
juy4556 <kws05050@naver.com>
2023-06-30 00:49:13 +0900

    feat: StockPrice엔티티 추가

---
+++ src/main/java/springBatch/domain/stock/entity/StockPrice.java
@@ -0,0 +1,31 @@
+package springbatch.domain.stock.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import springbatch.domain.entity.BaseEntity;
+import springbatch.domain.entity.StatusType;
+import springbatch.domain.stock.entity.Stock;
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StockPrice extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stock_id")
+    private Stock stock;
+    private Float price;
+
+    public StockPrice(Long id, Stock stock, Float price) {
+        super(StatusType.ACTIVATE.getStatus());
+        this.id = id;
+        this.stock = stock;
+        this.price = price;
+        stock.getStockPrices().add(this);
+    }
+}
