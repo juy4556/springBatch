98cba5fab64e5f1604e3db97584b8c966c18d2a2
juy4556 <kws05050@naver.com>
2023-06-30 00:47:58 +0900

    feat: 기본엔티티 추가 및 JPAAuditing 어노테이션 추가

---
+++ src/main/java/springBatch/domain/entity/BaseEntity.java
@@ -0,0 +1,34 @@
+package springbatch.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity implements Serializable {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private String status;
+
+    protected BaseEntity() {
+    }
+
+    protected BaseEntity(String status) {
+        this.status = status;
+    }
+}
