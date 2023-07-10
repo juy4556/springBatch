8528cf9ad4d96c7f6a5c959922125290c39f5571
juy4556 <kws05050@naver.com>
2023-07-10 23:34:15 +0900

    feature: 회원가입 시 password 암호화 기능 추가

--- src/main/java/springBatch/domain/user/service/UserService.java
+++ src/main/java/springBatch/domain/user/service/UserService.java
@@ -2,8 +2,18 @@ package springbatch.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import springbatch.domain.security.entity.RefreshToken;
+import springbatch.domain.security.jwt.JwtUtils;
+import springbatch.domain.security.service.RefreshTokenService;
+import springbatch.domain.user.dto.request.LoginRequest;
+import springbatch.domain.user.dto.response.LoginResponse;
 import springbatch.domain.user.dto.request.SignupRequest;
 import springbatch.domain.user.dto.response.SignupResponse;
 import springbatch.domain.user.entity.User;
@@ -17,10 +27,16 @@ import springbatch.domain.user.repository.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RefreshTokenService refreshTokenService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public SignupResponse signUp(SignupRequest request) {
-        User user = new User(request.getName(), request.getBirth(), request.getPassword());
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+        User user = new User(request.getName(), request.getBirth(), encodedPassword);
         userRepository.save(user);
         return new SignupResponse(user.getId(), user.getName());
     }
