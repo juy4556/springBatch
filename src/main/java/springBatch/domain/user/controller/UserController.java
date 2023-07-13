c6e453b56f6c34a4013c0591733319d5078cfc2d
juy4556 <kws05050@naver.com>
2023-07-13 23:44:45 +0900

    feat: 토큰 갱신 api 추가

--- src/main/java/springBatch/domain/user/controller/UserController.java
+++ src/main/java/springBatch/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package springbatch.domain.user.controller;
 
+import jakarta.transaction.NotSupportedException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,10 +9,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springbatch.domain.security.jwt.JwtAuth;
+import springbatch.domain.security.service.RefreshTokenService;
 import springbatch.domain.user.dto.request.LoginRequest;
+import springbatch.domain.user.dto.request.SignupRequest;
+import springbatch.domain.user.dto.request.TokenRefreshRequest;
 import springbatch.domain.user.dto.response.LoginResponse;
 import springbatch.domain.user.dto.response.SignupResponse;
-import springbatch.domain.user.dto.request.SignupRequest;
+import springbatch.domain.user.dto.response.TokenRefreshResponse;
 import springbatch.domain.user.service.UserService;
 
 @RestController
@@ -21,6 +26,7 @@ import springbatch.domain.user.service.UserService;
 public class UserController {
 
     private final UserService userService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/signup")
     public ResponseEntity<SignupResponse> register(@RequestBody @Valid final SignupRequest request) {
@@ -31,6 +37,14 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody @Valid final LoginRequest request) {
         LoginResponse loginResponse = userService.login(request);
+        return ResponseEntity.ok().body(loginResponse);
+    }
+
+    @PostMapping("/refresh/token")
+    public ResponseEntity<TokenRefreshResponse> login(@JwtAuth Long userId, @RequestBody TokenRefreshRequest request)
+            throws NotSupportedException {
+        String accessToken = request.getAccessToken();
+        TokenRefreshResponse loginResponse = refreshTokenService.refreshToken(userId, accessToken);
         return ResponseEntity.ok().body(loginResponse);
     }
 }
